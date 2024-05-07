e3e0fdf68da1baec743cd46ce9b54ce828414a52
MijoMercep <mijomercep1222@gmail.com>
2024-05-07 16:22:34 +0200

    Added textarea, fixed Date Picker, added button

--- savethelink/src/App.jsx
+++ savethelink/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 import { format } from 'date-fns';
@@ -7,50 +7,67 @@ import 'react-day-picker/dist/style.css';
 import "./newStyle.css";
 
 
-
-
+const today = new Date();
+const todayDate = today.toDateString();
+const initialDates = [];
 
 function App() {
-  const [selected, setSelected] = useState(new Date())
+  const [selected, setSelected] = useState(new Date());
+  const [dates, setDates] = useState(initialDates);
   const [css, setCss] = useState(`
-  .my-today { 
-    
-    background-color: white;
-    color: black;
-
-  }
-`);
-
-const changeColor = () => {
-  setCss(`
     .my-today { 
-      font-weight: bold;
-      font-size: 140%; 
-      color: white;
-      background-color: green;
+      background-color: white;
+      color: black;
     }
   `);
-};
-  
+  const [rerender, setRerender] = useState(false);
+  const [valueInput, setValueInput] = useState("");
+  useEffect(() => {
+    setCss(`
+      .my-today { 
+        font-weight: bold;
+        font-size: 140%; 
+        color: white;
+        background-color: green;
+      }
+    `);console.log(dates);
+  }, [rerender]); 
+
+  const changeColor = () => {
+    
+    if(selected.toDateString() == todayDate && valueInput != ""){
+      const updatedDates = [...dates, selected];
+      setDates(updatedDates);
+      setRerender(prev => !prev); 
+    }
+    
+    
+  };
+const valueOutput = () => {
+  console.log(valueInput);
+}
   return (
     <>
-    <style>{css}</style>
-      <DayPicker
-      
-      mode="single"
-      selected={selected}
-      
-      onSelect={setSelected}
-      
-      
-      modifiersClassNames={{ today : "my-today"  }}
-      
-      
-    />
-    
-    <button onClick={changeColor}>Save</button>
+      <div>
+        <style>{css}</style>
+        <DayPicker
+          mode="single"
+          selected={dates}
+          onSelect={setSelected}
+          modifiersClassNames={{
+            
+            selected: 'my-today',
+          }}
+        />
+        <div className='button-div'><button onClick={changeColor}>Save</button></div>
+        <button onClick={valueOutput}>Ispis</button>
+      </div>
+      <div className='input-div'>
+        <textarea className='text-input' placeholder='Type your achievement...' value={valueInput} onChange={e => setValueInput(e.target.value)}></textarea>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
