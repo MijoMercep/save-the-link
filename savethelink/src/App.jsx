991853146173b2c14bd5f2a2a082fe892c300d55
MijoMercep <mijomercep1222@gmail.com>
2024-05-08 20:13:44 +0200

    Fixed bugs with Date Picker, added longest streak, added current streak

--- savethelink/src/App.jsx
+++ savethelink/src/App.jsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react'
-
-import './App.css'
-import { format } from 'date-fns';
-import { Button, DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
+import { useState, useEffect } from "react";
+import { summary } from 'date-streaks';
+import "./App.css";
+import { format } from "date-fns";
+import { Button, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 import "./newStyle.css";
-
 
 const today = new Date();
 const todayDate = today.toDateString();
@@ -25,8 +24,6 @@ function App() {
   const [dateOutput, setDateOutput] = useState(new Map());
   const mapArray = [];
 
-
-
   useEffect(() => {
     setCss(`
       .my-today { 
@@ -35,57 +32,80 @@ function App() {
         color: white;
         background-color: green;
       }
-    `);console.log(dates);
-  }, [rerender]); 
+    `);
+  }, [rerender]);
 
   const changeColor = () => {
+    if(valueInput != ""){
+      try{
+        const formattedDate = selected.toDateString();
+        const newMap = new Map(dateOutput);
+        newMap.set(selected?.toDateString(), valueInput);
+        setDateOutput(newMap);
+        mapArray.push(newMap);
+        if (!dates.includes(formattedDate)) {
+          const updatedDates = [...dates, formattedDate];
+          setDates(updatedDates);
+        }
     
-    
-      const newMap = new Map(dateOutput);
-      newMap.set(selected?.toDateString(), valueInput);
-      setDateOutput(newMap);
-      mapArray.push(newMap);
-      console.log(dateOutput);
-      console.log(mapArray);  
-      const updatedDates = [...dates, selected];
-      setDates(updatedDates);
-      setRerender(prev => !prev); 
-      
-   
+        
+        setRerender((prev) => !prev);}
+        catch(error){
+          if(error instanceof TypeError){
+            window.alert("Select a date");
+          }    }
+    }
     
     
   };
-const valueOutput = () => {
-  console.log(valueInput);
-  console.log(dateOutput);
-  console.log(selected);
-  console.log(dates);
-  console.log(selected?.toDateString());
-  console.log(dateOutput.get(selected?.toDateString()));
-  
-}
+
+  const valueOutput = () => {
+    console.log(valueInput);
+    console.log(dateOutput);
+    console.log(selected);
+    console.log(today.setDate(today.getDate()+1))
+    console.log(dates);
+    console.log(selected?.toDateString());
+    console.log(dateOutput.get(selected.toDateString()));
+    console.log(dates.slice(-1));
+    console.log(summary({ dates }))
+    dates.forEach(element => {
+      console.log(element);
+    });
+  };
 
   return (
     <>
       <div>
         <style>{css}</style>
-        
+
         <DayPicker
           mode="single"
-          selected={dates[-1]}
+          selected={selected}
           onSelect={setSelected}
+          modifiers={{
+            myToday: (date) => dates.includes(date.toDateString()),
+          }}
           modifiersClassNames={{
-            
-            selected: 'my-today',
+            myToday: "my-today",
           }}
         />
-        <div className='button-div'><button onClick={changeColor}>Save</button></div>
+        <div className="button-div">
+          <button onClick={changeColor}>Save</button>
+        </div>
         <button onClick={valueOutput}>Ispis</button>
       </div>
-      <div className='input-div'>
-        <textarea className='text-input' placeholder='Type your achievement...' value={valueInput} onChange={e => setValueInput(e.target.value)}></textarea>
-          <p>{format(selected, 'PPP')}</p>
-          <p>{dateOutput.get(selected?.toDateString())}</p>
+      <div className="input-div">
+        <textarea
+          className="text-input"
+          placeholder="Type your achievement..."
+          value={valueInput}
+          onChange={(e) => setValueInput(e.target.value)}
+        ></textarea>
+        <p>Your message on: {selected?.toDateString()}</p>
+        <p>{dateOutput.get(selected?.toDateString())}</p>
+        <p>Current Streak: {summary(dates).currentStreak}</p>
+        <p>Longest Streak: {summary(dates).longestStreak}</p>
       </div>
     </>
   );
