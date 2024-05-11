5e35ce2c172e500a22638472547239216433a40d
MijoMercep <mijomercep1222@gmail.com>
2024-05-11 17:41:57 +0200

    styled the front page

--- savethelink/src/MainPage.jsx
+++ savethelink/src/MainPage.jsx
@@ -22,7 +22,7 @@ function MainPage({logout, user}){
   const [dates, setDates] = useState([]);
   const [css, setCss] = useState(`
     .my-today { 
-      background-color: white;
+      background-color: black;
       color: black;
     }
   `);
@@ -93,8 +93,9 @@ useEffect(() => {
       .my-today { 
         font-weight: bold;
         font-size: 140%; 
-        color: white;
-        background-color: green;
+        color: black;
+        
+        text-decoration: line-through;
       }
     `);
   }, [rerender]);
@@ -127,30 +128,20 @@ useEffect(() => {
     
   };
 
-  const valueOutput = () => {
-    console.log(data.mapField)
-    console.log(dateOutput);
-   
-   
-    console.log(dates);
-    
-    console.log(dateOutput.get(selected.toDateString()));
-    
-    console.log(user?.email);
-    console.log(user.uid)
-    console.log(user);
-    
-    dates.forEach(element => {
-      console.log(element);
-    });
-  };
+ 
 
     return(
         <>
-        <div>
-
+        <div className = "header">
+          <h1>Don't break the chain!</h1>
+<h2>Save the habit</h2>
+        <svg className="header-curve" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#CDE1FF" fill-opacity="1" d="M0,224L80,218.7C160,213,320,203,480,213.3C640,224,800,256,960,245.3C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+        
+        </div>
+        <img className="header-profile" src="/images/profile-svg.svg"></img>
+        <div className="body-container">
         <style>{css}</style>
-        <p>User logged in: {user?.email}</p>
+        
         <DayPicker
           mode="single"
           selected={selected}
@@ -163,23 +154,39 @@ useEffect(() => {
           }}
         />
         <div className="button-div">
-          <button onClick={changeColor}>Save</button>
+        <button onClick={changeColor} className="save-button">Save</button>
         </div>
-        <button onClick={valueOutput}>Ispis</button>
-      </div>
+        
+      
       <div className="input-div">
         <textarea
           className="text-input"
           placeholder="Type your achievement..."
           value={valueInput}
           onChange={(e) => setValueInput(e.target.value)}
-        ></textarea>
-        <p>Your message on: {selected?.toDateString()}</p>
-        <p>{dateOutput.get(selected?.toDateString())}</p>
-        <p>Current Streak: {summary(dates).currentStreak}</p>
-        <p>Longest Streak: {summary(dates).longestStreak}</p>
-        <button onClick={logout}>Logout</button>
-      </div></>
+        >
+
+        </textarea>
+        
+      </div>
+      <div className="body-container-output">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#CDE1FF" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+        <p className="message-output">Your message on: {selected?.toDateString()}</p>
+        <span className="span-container"><p className="message-output">{dateOutput.get(selected?.toDateString())}</p>
+        </span>
+        <div className="body-footer">
+          <div className="body-footer-current">
+            <p>Current Streak:</p>
+            <p> {summary(dates).currentStreak}</p>
+          </div>
+          
+          <div className="body-footer-longest"><p>Longest Streak:</p>
+          <p> {summary(dates).longestStreak}</p></div>
+          <button onClick={logout}>Logout</button>
+        </div>
+        
+        </div></div></>
+      
     )
 }
 
