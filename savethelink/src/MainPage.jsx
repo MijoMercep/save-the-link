be6c0ed808466a797566e11c3031dc3f89f46248
MijoMercep <mijomercep1222@gmail.com>
2024-05-13 23:03:24 +0200

    added extra functionality to main page, as well as umproved UI

--- savethelink/src/MainPage.jsx
+++ savethelink/src/MainPage.jsx
@@ -128,6 +128,8 @@ useEffect(() => {
     
   };
 
+  
+    
  
 
     return(
@@ -161,8 +163,9 @@ useEffect(() => {
       <div className="input-div">
         <textarea
           className="text-input"
-          placeholder="Type your achievement..."
+          
           value={valueInput}
+          placeholder={dateOutput.get(selected?.toDateString()) || "Type your achievement..."}
           onChange={(e) => setValueInput(e.target.value)}
         >
 
@@ -170,10 +173,9 @@ useEffect(() => {
         
       </div>
       <div className="body-container-output">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#CDE1FF" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-        <p className="message-output">Your message on: {selected?.toDateString()}</p>
-        <span className="span-container"><p className="message-output">{dateOutput.get(selected?.toDateString())}</p>
-        </span>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="footer-curve"><path fill="#CDE1FF" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="footer-curve-2"><path fill="#232b2b" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+
         <div className="body-footer">
           <div className="body-footer-current">
             <p>Current Streak:</p>
