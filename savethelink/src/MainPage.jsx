3e0193aa0a83d810d7a398f46ad095d8945e4dd1
MijoMercep <mijomercep1222@gmail.com>
2024-05-20 22:44:20 +0200

    Added logout functionality and changed the footer

--- savethelink/src/MainPage.jsx
+++ savethelink/src/MainPage.jsx
@@ -17,8 +17,8 @@ const todayDate = today.toDateString();
 
 function MainPage({logout, user}){
   const initialDates = [];
-
-    const [selected, setSelected] = useState(new Date());
+  const [isProfileVisible, setProfileVisible] = useState(false);
+  const [selected, setSelected] = useState(new Date());
   const [dates, setDates] = useState([]);
   const [css, setCss] = useState(`
     .my-today { 
@@ -140,8 +140,12 @@ useEffect(() => {
         <svg className="header-curve" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#CDE1FF" fill-opacity="1" d="M0,224L80,218.7C160,213,320,203,480,213.3C640,224,800,256,960,245.3C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
         
         </div>
-        <img className="header-profile" src="/images/profile-svg.svg"></img>
+        <img className="header-profile" src="/images/profile-svg.svg" onMouseEnter={() => setProfileVisible(true)}></img>
+        <div className={`profile-show ${isProfileVisible ? 'visible' : ''}`}
+        onMouseLeave={() => setProfileVisible(false)}><p>User: {user.email}</p>
+        <button onClick={logout} className="logout-btn">Logout</button></div>
         <div className="body-container">
+        
         <style>{css}</style>
         
         <DayPicker
@@ -185,9 +189,11 @@ useEffect(() => {
           <div className="body-footer-longest"><p>Longest Streak:</p>
           <p> {summary(dates).longestStreak}</p></div>
           <button onClick={logout}>Logout</button>
+          <p className="description-text">Welcome to Don't Break the Chain, the ultimate tool to help you stay motivated and productive every day! Our web app is designed to encourage consistency and foster a sense of accomplishment by allowing you to log your daily achievements.</p>
           <p className="built-by-text">Built by <a className="name-text">Mijo</a> |</p>
           <a href="https://github.com/MijoMercep">
           <img className="github-logo" src="/images/github.svg"></img></a>
+
         </div>
         
         </div></div></>
