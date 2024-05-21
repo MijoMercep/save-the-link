eff17dc7f9c14a7b8feb0b1e4a15d56fbbff0788
MijoMercep <mijomercep1222@gmail.com>
2024-05-21 22:54:17 +0200

    Finished up the design for login page

--- savethelink/src/LoginPage.jsx
+++ savethelink/src/LoginPage.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import "./App.css";
 function LoginPage({ setLoginEmail, setLoginPassword, login, signup}){
   return(<>
-      <h2>MainPage</h2>
+      
       <div>
-      <div className="body-container-output" >
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="footer-curve"><path fill="#CDE1FF" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="footer-curve-2"><path fill="#232b2b" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,154.7C480,171,600,213,720,218.7C840,224,960,192,1080,186.7C1200,181,1320,203,1380,213.3L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+      <div className="header">
+      <h1>Don't break the chain!</h1>
+<h2>Save the habit</h2>
+<svg xmlns="http://www.w3.org/2000/svg" className="header-curve-login" viewBox="0 0 1440 320"><path fill="#CDE1FF" fill-opacity="1" d="M0,32L60,37.3C120,43,240,53,360,101.3C480,149,600,235,720,234.7C840,235,960,149,1080,138.7C1200,128,1320,192,1380,224L1440,256L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
        </div>
-        <h3>Login</h3>
+
+       
+        <div className="body-login">
         
-        <input placeholder="email" onChange={(event) => {setLoginEmail(event.target.value)}}></input>
-        <input placeholder="password" onChange={(event) => {setLoginPassword(event.target.value)}}></input>
+        <h1 className="login-header">Login</h1>
+          <div className="div-input">
+        <input placeholder="E-Mail" onChange={(event) => {setLoginEmail(event.target.value)}} className="input-type"></input>
+        </div>
+        <div className="div-input">
+        <input placeholder="Password" type="password"onChange={(event) => {setLoginPassword(event.target.value)}} className="input-type"></input>
+        </div>
+       
         
-          
-          <button onClick={login}>Login</button>
-          <button onClick={signup}>Sign Up</button>
+          <div className="div-login-btn"><button onClick={login} className="login-btn">Login</button></div>
+          <div className="register-text"><p>Don't have an account? <a onClick={signup}>Register</a></p></div>
+          </div>
       </div></>
   );
   
